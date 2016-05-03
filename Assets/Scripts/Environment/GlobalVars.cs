1d1ffe5f292ae5ee1b1230a5d650876b36933350
Tung_Nguyen <7319tnguyen040@gmail.com>
2016-05-03 16:39:50 -0700

    Tung's scene, controller controller

---
+++ Assets/Scripts/Environment/GlobalVars.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+/*
+--------------------------------
+Author: Tung Nguyen
+
+Purpose: Like the LevelTracker script in which it stores data for the gameobjects to use except
+storing it here will save the data to be used on the next loaded scene also. Many scripts will
+rely on this script.
+
+Script communicate with: LevelTracker(For spawning the object this script is attached to, not
+directly)
+
+Used by: Almost everything
+
+Last edited: Tung Nguyen
+--------------------------------
+*/
+
+public class GlobalVars : MonoBehaviour
+{
+    //Misc
+    public bool Save = false;
+    public bool Load = false;
+    public Vector3 CurrentPosition = new Vector3(0, 0, 0);
+
+    //Tests
+    public bool DebugMode = false;
+
+    public string MainMenu;
+
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Update()
+    {
+        //Debug Toggle
+        if (Input.GetButtonDown("Debug"))
+        {
+            if (DebugMode == true) DebugMode = false;
+            else DebugMode = true;
+        }
+
+        //Reload the scene
+        if (Input.GetButtonDown("Home")) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        //Back to main menu
+        if (Input.GetButtonDown("Pause"))
+        {
+            SceneManager.LoadScene(MainMenu);
+        }
+    }
+}
---
+++ Assets/Scripts/Vive/StickController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class StickController : MonoBehaviour
+{
+    //Get Input keys
+    private Valve.VR.EVRButtonId GripyButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId TriggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    //Get the controller
+    private SteamVR_Controller.Device Controller { get { return SteamVR_Controller.Input((int)TrackedObj.index); } }
+    private SteamVR_TrackedObject TrackedObj;
+
+    private GameObject GrabedObject;
+
+    private Vector3 lastPos;
+
+    [HideInInspector]
+    public Vector3 vel;
+
+
+    void Start ()
+    {
+        TrackedObj = GetComponent<SteamVR_TrackedObject>();
+        lastPos = transform.position;
+	}
+
+	void Update ()
+    {
+	    if(Controller == null)
+        {
+            Debug.Log("Controller is not initialized");
+            return;
+        }
+
+        vel = (transform.position - lastPos) / Time.deltaTime;
+        lastPos = transform.position;
+
+        ManageInputs();
+    }
+
+    //Interactions
+    void ManageInputs()
+    {
+        //Grab
+        if(GrabedObject != null)
+        {
+            if (Controller.GetPress(GripyButton) && GrabedObject != null)
+            {
+                if (GrabedObject.GetComponent<Rigidbody>() != null) GrabedObject.GetComponent<Rigidbody>().isKinematic = true;
+                GrabedObject.transform.parent = transform; //Turns to a child also
+            }
+            else
+            {
+                GrabedObject.transform.SetParent(null);
+                GrabedObject.GetComponent<Rigidbody>().isKinematic = false;
+                GrabedObject.GetComponent<Rigidbody>().AddForce(vel * 100);
+                Debug.Log(vel);
+                GrabedObject = null;
+            }
+
+        }
+        //DebugInputs();
+    }
+
+    //Test a few buttons
+    void DebugInputs()
+    {
+        if (Controller.GetPressDown(GripyButton)) print("Grip Button Down");
+        if (Controller.GetPressUp(GripyButton)) print("Grip Button Up");
+        if (Controller.GetPress(GripyButton)) print("Grip Button Pressed"); //Held
+
+        if (Controller.GetPressDown(TriggerButton)) print("Trigger Button Down");
+        if (Controller.GetPressUp(TriggerButton)) print("Trigger Button Up");
+        if (Controller.GetPress(TriggerButton)) print("Trigger Button Pressed"); //held
+    }
+
+    //Collisions
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Grabable")) GrabedObject = other.gameObject;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        //if (other.gameObject.layer == LayerMask.NameToLayer("Grabable")) GrabedObject = other.gameObject;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //if (other.gameObject.layer == LayerMask.NameToLayer("Grabable")) GrabedObject = null;
+    }
+}
